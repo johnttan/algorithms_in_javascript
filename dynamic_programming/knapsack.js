047b1e877cfc49682c9c8352b56826603b66b2bc
John Tan <tantai.john@gmail.com>
2015-04-08 15:22:25 -0700

    started knapsack_dp

--- dynamic_programming/knapsack.js
+++ dynamic_programming/knapsack.js
@@ -55,11 +55,25 @@ function knapsack_recursive_memoized(currentItem, weights, values, weightLeft, m
   return res;
 }
 
+function knapsack_dp(weights, values, maxWeight){
+  var table = [];
+  // Construct base table with 0 values
+  // Consider item = 0 to correspond with table[1]
+  for(var i=0;i<=values.length;i++){
+    table[i] = [];
+    for(var j=0;j<=maxWeight;j++){
+      table[j] = 0;
+    }
+  }
+
+
+}
+
 var testWeights = [];
 var testValues = [];
-for(var i=0;i<50;i++){
+for(var i=0;i<70;i++){
   testWeights.push(Math.floor((Math.random() * Math.random() * 10 / Math.random())));
   testValues.push(Math.floor((Math.random() * Math.random() * 10 / Math.random())));
 }
-timing("knapsack_recursive", knapsack_recursive, [testValues.length-1, testWeights, testValues, 11])
-timing("knapsack_recursive_memoized", knapsack_recursive_memoized, [testValues.length-1, testWeights, testValues, 11, {}])
+timing("KNAPSACK_RECURSIVE", knapsack_recursive, [testValues.length-1, testWeights, testValues, 11])
+timing("KNAPSACK_RECURSIVE_MEMOIZED", knapsack_recursive_memoized, [testValues.length-1, testWeights, testValues, 11, {}])
