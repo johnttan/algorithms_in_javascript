8c94a6e96f586eba45b34d15a9116a329d749112
John Tan <tantai.john@gmail.com>
2015-04-07 23:22:35 -0700

    dp solution added

--- dynamic_programming/cut_rods.js
+++ dynamic_programming/cut_rods.js
@@ -16,6 +16,7 @@ var res = cut_rods([0, 1, 5, 8, 9, 10, 17, 20, 24, 30, 1, 2, 3, 4, 1, 2, 3, 2, 4
 var end = Date.now();
 console.log(res, end - start);
 
+// O(n^2)
 function cut_rods_memoized(prices, length){
   var table = {};
   function cut_rods(prices, length){
@@ -42,5 +43,23 @@ function cut_rods_memoized(prices, length){
 
 var start = Date.now();
 var res = cut_rods_memoized([0, 1, 5, 8, 9, 10, 17, 20, 24, 30, 1, 2, 3, 4, 1, 2, 3, 2, 4, 15, 20, 14, 123, 123,123,1 , 32, 21, 14, 23, 14, 23, 41, 123, 4, 23], 25);
+var end = Date.now();
+console.log(res, end - start);
+
+// O(n^2)
+function cut_rods_dp(prices, length){
+  var table = [0];
+  for(var i=1;i<=length;i++){
+    var max = Number.NEGATIVE_INFINITY;
+    for(var j=1;j<=i;j++){
+        max = Math.max(max, prices[j] + table[i-j])
+    }
+    table[i] = max;
+  }
+  return table[length];
+}
+
+var start = Date.now();
+var res = cut_rods_dp([0, 1, 5, 8, 9, 10, 17, 20, 24, 30, 1, 2, 3, 4, 1, 2, 3, 2, 4, 15, 20, 14, 123, 123,123,1 , 32, 21, 14, 23, 14, 23, 41, 123, 4, 23], 25);
 var end = Date.now();
 console.log(res, end - start);
